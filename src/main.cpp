2b2f6c4d73cc1a5cc7b1b8a2d6fa98640feb1160
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2025-07-29 23:20:10 +0900

    Use function

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
-int main() {
+void hello_world() {
     std::cout << "Hello, World" << std::endl;
+}
+
+void show_sum(int a, int b) {
+    int c = a + b;
+    std::cout << c << std::endl;
+}
+
+int main() {
+    hello_world();
+    show_sum(3, 4);
 }
