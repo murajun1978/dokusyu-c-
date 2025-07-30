42a67a4fb40c18189c7bf02f33151ba584904c3c
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2025-07-30 21:23:31 +0900

    Use class

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,64 @@
 #include <iostream>
 
-struct product {
+class product
+{
     int id;
     int price;
     int stock;
+
+public:
+    int get_id();
+    void set_id(int new_id);
+
+    int get_price();
+    void set_price(int new_price);
+
+    int get_stock();
+    void set_stock(int new_stock);
 };
+
+int product::get_id() {
+    return id;
+}
+
+void product::set_id(int new_id) {
+    id = new_id;
+}
+
+int product::get_price() {
+    return price;
+}
+
+void product::set_price(int new_price) {
+    if (new_price < 0) {
+        std::cerr << "価格は負の値にできません。" << std::endl;
+        return;
+    }
+    price = new_price;
+}
+
+int product::get_stock() {
+    return stock;
+}
+
+void product::set_stock(int new_stock) {
+    if (new_stock < 0) {
+        std::cerr << "在庫数は負の値にできません。" << std::endl;
+        return;
+    }
+    stock = new_stock;
+}
 
 int main() {
     product pen;
 
-    pen.id = 1;
-    pen.price = 100;
-    pen.stock = 200;
+    pen.set_id(0);
+    pen.set_price(100);
+    pen.set_stock(200);
 
     product* ptr = &pen;
 
-    std::cout << "商品ID: " << ptr->id << std::endl;
-    std::cout << "単価: " << ptr->price << std::endl;
-    std::cout << "在庫数: " << ptr->stock << std::endl;
+    std::cout << "商品ID: " << ptr->get_id() << std::endl;
+    std::cout << "単価: " << ptr->get_price() << std::endl;
+    std::cout << "在庫数: " << ptr->get_stock() << std::endl;
 }
